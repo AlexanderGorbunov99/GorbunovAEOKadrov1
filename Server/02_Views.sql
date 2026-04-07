524ea0b58dbf74a2ae64614b1623104d40ea7235
Горбунов Александр <gorbunovae99@ro.ru>
2026-04-07 21:48:41 +0800

    Create 02_Views.sql

---
+++ Server/02_Views.sql
@@ -0,0 +1,8 @@
+CREATE VIEW [dbo].[otdel_View]
+	AS SELECT * FROM [dbo].[Otdel]
+
+  CREATE VIEW [dbo].[Sotrudnik_View]
+	AS SELECT * FROM [dbo].[Sotrudnik]
+
+  CREATE VIEW [dbo].[Dety_View]
+	AS SELECT * FROM [dbo].[Dety]
